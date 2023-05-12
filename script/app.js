98ff2673491793409f4d20ac9a3dfb39effba6d7
FacundoEsquivel97 <esquivelfacundo.97@gmail.com>
2023-05-12 18:15:45 -0300

    Update!
    Se randerizan los post haciendo uso de una paginación

--- script/app.js
+++ script/app.js
@@ -1,33 +1,56 @@
 document.addEventListener('DOMContentLoaded',()=>{
     const postContainer = document.querySelector('.post-container');
     const postTemplate = document.querySelector('.post-template').content;
+    const pagination = document.querySelector('.pagination');
+
+    let pageNumber = 0
+    const postsPerPage = 6;
 
     const peticion = (endpoint,func) => {
         fetch('https://jsonplaceholder.typicode.com/'+endpoint)
         .then((response) => response.json())
-        .then((json) => {func(json)})
+        .then((res) => {func(res)})
         .catch((error)=> {console.log(error)})
     }
     const renderPost = (data) =>{
+        if (data !== undefined) {
         const clone = postTemplate.cloneNode(true);
         const fragment = document.createDocumentFragment();
+        let cloneUser = clone.querySelector('.post-user')
         clone.querySelector('.post-title').textContent = data.title ;
         clone.querySelector('.post-body').textContent = data.body ;
-        peticion(`users/${data.userId}`,(user) => {
-            clone.querySelectorAll('.post-user')[data.userId-1].textContent = user.username
-        })
+        peticion(`users/${data.userId}`,(user) => 
+        {cloneUser.textContent = user.username})
         fragment.appendChild(clone);
         postContainer.appendChild(fragment);
+        }
     }
 
-peticion(`posts`,(postsCount)=>{
-    postsCount = postsCount.sort(() => Math.random() - 0.5) 
-    console.log(postsCount.length)
-
- for (let i = 1; i < postsCount.length; i += 1) {
-      renderPost(postsCount[i])
-      console.log(postsCount[i])
+    const handleNextPage = (index,postsList) => {
+        pageNumber = index
+        while (postContainer.lastChild) {postContainer.removeChild(postContainer.lastChild)}
+        renderPage(postsList)
     }
+
+    const renderPage = (postsList) =>{
+        let numberOfPages = Math.ceil(postsList.length / postsPerPage) ;
+        for (let i = pageNumber*postsPerPage; i < (postsPerPage+(pageNumber*postsPerPage)); i ++) {
+          renderPost(postsList[i])
+        }
+        if (!pagination.hasChildNodes()) {
+            for (let index= 0 ; index <= numberOfPages - 1; index ++) {
+                const nextPageButton = document.createElement('button');
+                nextPageButton.textContent = index + 1
+                nextPageButton.addEventListener('click',() => {handleNextPage(index,postsList)});
+                pagination.appendChild(nextPageButton)}
+        }
+        }
+
+peticion(`posts`, (postsList)=> {
+    postsList = postsList.sort(() => Math.random() - 0.5) 
+    renderPage(postsList)
 })
-
 });
+
+
+
