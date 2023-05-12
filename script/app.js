5f51e69eb7dcb294458f1dbfa01f9371236599dc
FacundoEsquivel97 <esquivelfacundo.97@gmail.com>
2023-05-12 16:10:00 -0300

    codigo reestructurado

--- script/app.js
+++ script/app.js
@@ -14,23 +14,20 @@ document.addEventListener('DOMContentLoaded',()=>{
         clone.querySelector('.post-title').textContent = data.title ;
         clone.querySelector('.post-body').textContent = data.body ;
         peticion(`users/${data.userId}`,(user) => {
-            document.querySelectorAll('.post-user')[data.userId-1].textContent = user.username
+            clone.querySelectorAll('.post-user')[data.userId-1].textContent = user.username
         })
         fragment.appendChild(clone);
         postContainer.appendChild(fragment);
     }
 
-const sumarPost = () => {
-    let postsCount = localStorage.getItem("postsCount") || 100;
-    postsCount = JSON.parse(postsCount);
-    postsCount ++;
-    localStorage.setItem("postsCount", JSON.stringify(postsCount));
-}
+peticion(`posts`,(postsCount)=>{
+    postsCount = postsCount.sort(() => Math.random() - 0.5) 
+    console.log(postsCount.length)
 
- for (let i = 1; i < postsCount; i += 10) {
-      peticion(`posts/${i}`,renderPost)
+ for (let i = 1; i < postsCount.length; i += 1) {
+      renderPost(postsCount[i])
+      console.log(postsCount[i])
     }
-
-
+})
 
 });
